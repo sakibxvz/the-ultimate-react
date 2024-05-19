012497ee87bbb7d38661dfb777f7beb7f0dffdde
sakibxvz <sakibxvz@gmail.com>
2024-05-19 14:07:28 +0600

    App v-1 added

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,27 +1,27 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-import StarRating from './StarRating';
+import './index.css';
+import App from './App';
+// import StarRating from './StarRating';
 
 
-function Test() {
-	const [movieRating, setMovieRating] = useState(0)
-	return (
-		<div>
-			<StarRating color='blue' maxRating={10} onSetRating={setMovieRating} />
-			<p>This movie was rated {movieRating} stars</p>
-		</div>
-	);
-}
+// function Test() {
+// 	const [movieRating, setMovieRating] = useState(0)
+// 	return (
+// 		<div>
+// 			<StarRating color='blue' maxRating={10} onSetRating={setMovieRating} />
+// 			<p>This movie was rated {movieRating} stars</p>
+// 		</div>
+// 	);
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		{/* <App /> */}
-		<StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} />
+		<App />
+		{/* <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} />
 		
 		<StarRating maxRating={10} className='test' defaultRating={5} />
-		<Test/>
+		<Test/> */}
 	</React.StrictMode>
 );
