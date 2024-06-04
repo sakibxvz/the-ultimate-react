340399e6929f327222fb2f88221da84300f8a6f2
sakibxvz <sakibxvz@gmail.com>
2024-06-04 19:00:13 +0600

    All Functionality Done

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
@@ -9,6 +9,8 @@ import PageNotFound from './pages/PageNotFound';
 import Login from './pages/Login';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
+import City from './components/City';
+import Form from './components/Form';
 
 const BASE_URL = 'http://localhost:9000';
 function App() {
@@ -22,7 +24,7 @@ function App() {
 				const res = await fetch(`${BASE_URL}/cities`);
 				const data = await res.json();
 				setCities(data);
-				console.log(data)
+				console.log(data);
 			} catch {
 				alert('There is Error in loading the data');
 			} finally {
@@ -40,19 +42,17 @@ function App() {
 				<Route path='pricing' element={<Pricing />} />
 				<Route path='login' element={<Login />} />
 				<Route path='app' element={<AppLayout />}>
-					<Route
-						index
-						element={<CityList cities={cities} isLoading={isLoading} />}
-					/>
+					<Route index element={<Navigate replace to='cities' />} />
 					<Route
 						path='cities'
 						element={<CityList cities={cities} isLoading={isLoading} />}
 					/>
+					<Route path='cities/:id' element={<City />} />
 					<Route
 						path='countries'
 						element={<CountryList cities={cities} isLoading={isLoading} />}
 					/>
-					<Route path='form' element={<p>Form</p>} />
+					<Route path='form' element={<Form />} />
 				</Route>
 				<Route path='*' element={<PageNotFound />} />
 			</Routes>
--- 11-worldwise/src/components/CountryList.jsx
+++ 11-worldwise/src/components/CountryList.jsx
@@ -1,7 +1,7 @@
-import CityItem from './CityItem';
-import styles from './CountryList.module.css'
+import styles from './CountryList.module.css';
 import Spinner from './Spinner';
 import Message from './Message';
+import CountryItem from './CountryItem';
 
 function CountryList({ cities, isLoading }) {
 	if (isLoading) return <Spinner />;
@@ -9,10 +9,19 @@ function CountryList({ cities, isLoading }) {
 		return (
 			<Message message='Add your first city by clicking on a city on the map' />
 		);
+
+	const countries = cities.reduce((arr, city) => {
+		if (!arr.map((el) => el.country).includes(city.country)) {
+			return [...arr, { country: city.country, emoji: city.emoji }];
+		} else {
+			return arr;
+		}
+	},[]);
+
 	return (
 		<ul className={styles.countryList}>
-			{cities.map((city) => (
-				<CityItem key={city.id} city={city} />
+			{countries.map((country) => (
+				<CountryItem key={country.country} country={country} />
 			))}
 		</ul>
 	);
