ece457b2c5d1bca2b363aba09d3f94a7f95ea3d1
sakibxvz <sakibxvz@gmail.com>
2024-06-23 18:54:04 +0600

    308

---
+++ 15-fast-react-pizza/src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold md:block">Sakib</div>;
+}
+
+export default Username;
--- 15-fast-react-pizza/src/ui/AppLayout.jsx
+++ 15-fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,23 +1,25 @@
-import { Outlet, useNavigation } from 'react-router-dom';
-import CartOverview from '../features/cart/CartOverview';
-import Header from './Header';
-import Loader from './Loader';
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+import Loader from "./Loader";
 
 function AppLayout() {
-	const navigation = useNavigation();
-	const isLoading = navigation.state === 'loading';
-	return (
-		<div className='layout'>
-			{isLoading && <Loader />}
-			<Header />
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
+      <Header />
 
-			<main>
-				<h1>Conent</h1>
-				<Outlet />
-			</main>
-			<CartOverview />
-		</div>
-	);
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl ">
+          {/* <h1>Conent</h1> */}
+          <Outlet />
+        </main>
+      </div>
+      <CartOverview />
+    </div>
+  );
 }
 
 export default AppLayout;
