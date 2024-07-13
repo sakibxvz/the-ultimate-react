86b913bd125523fa2ab96ae57f83e24378d5b2ba
sakibxvz <sakibxvz@gmail.com>
2024-07-13 09:38:33 +0600

    Transtek Query Intrigated

--- 16-the-wild-oasis/src/services/apiCabins.js
+++ 16-the-wild-oasis/src/services/apiCabins.js
@@ -5,7 +5,17 @@ export async function getCabins() {
 
 	if (error) {
 		console.error(error);
-		throw new Error('Cabins cloud nt be loaded');
+		throw new Error('Cabins cloud not be loaded');
+	}
+	return data;
+}
+
+export async function deleteCabin(id) {
+	const { data, error } = await supabase.from('cabins').delete().eq('id', id);
+
+	if (error) {
+		console.error(error);
+		throw new Error('Cabins cloud not be deleted');
 	}
 	return data;
 }
